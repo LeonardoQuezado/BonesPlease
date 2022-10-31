506e8c82a7b596d759b5ff6c551636b6c7ba76e4
JLMSC <reecaptcha@gmail.com>
2022-10-31 16:20:23 -0300

    Organização de arquivos, bug fixes e implementação base dos 'documentos'.

--- Jogo/Game/Game.gd
+++ Jogo/Game/Game.gd
@@ -1,12 +1,5 @@
 extends Control
 
-# 
 func _ready():
-	print("TODO: Game.gd apos transicao, fade in mostrar texto explicando a historia e ir pro jogo")
 	print("TODO: Game.gd apos explicar a historia comecar o jogo")
 	print("TODO: Game.gd tutorial basico de como jogar")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
